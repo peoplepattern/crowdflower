3e2118356b19847a849f3b8cdc2368ce7e33a35c
Christopher Brown <io@henrian.com>
2014-08-05 14:36:16 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='crowdflower',
-    version='0.0.11',
+    version='0.0.12',
     author='Christopher Brown',
     author_email='io@henrian.com',
     url='https://github.com/peoplepattern/crowdflower',
