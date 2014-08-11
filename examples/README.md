eb0db07b99eedbd7bd48cf303e222679d83b4aea
Christopher Brown <io@henrian.com>
2014-08-11 13:02:44 -0500

    Finish out documentation for example and add example results

--- examples/README.md
+++ examples/README.md
@@ -8,6 +8,20 @@ Launch it:
 
     python spam.py launch
 
+Check the status of the job (it's done when `needed_judgments` is 0):
+
+    python spam.py ping
+
+Cross-tabulate the results:
+
+    python spam.py results | cut -d$'\t' -f 1-2 | sort | uniq -c
+
+Download the full dataset:
+
+    python spam.py download > spam.json
+
+I have removed the `_worker_id` fields from the [`spam.json`](spam.json) file included in this repository, but otherwise that file was built using the above process.
+
 And after getting all the responses and downloading the results, delete it:
 
     python spam.py delete
--- examples/spam.py
+++ examples/spam.py
@@ -1,5 +1,9 @@
 import os
+import sys
+import json
+import random
 import crowdflower
+from crowdflower.exception import CrowdFlowerError
 
 # expects api key to be available in your environment variables; does not use cache
 conn = crowdflower.Connection()
@@ -10,6 +14,7 @@ def _find_job():
     for job in conn.jobs():
         if job_tag in job.tags:
             return job
+
 
 def create():
     filename = os.path.join(crowdflower.root, 'examples', 'spam.txt')
@@ -31,12 +36,14 @@ def create():
     # add the spam_gold key (to trigger)
     for datum in gold_data:
         datum['spam_gold'] = datum['label']
+    random.shuffle(gold_data)
 
     # for real data, get 100 of each class, and rename the special 'spam_gold' field
     test_data = ham_data[25:][:100] + spam_data[25:][:100]
+    random.shuffle(test_data)
 
     job = conn.upload(gold_data + test_data)
-    print 'Uploaded %d gold data, %d test data' % (len(gold_data), len(test_data))
+    print >> sys.stderr, 'Uploaded %d gold data, %d test data' % (len(gold_data), len(test_data))
 
     job.update({
         'title': 'Spam detection',
@@ -45,47 +52,68 @@ def create():
         'judgments_per_unit': 2,
         'payment_cents': 10,
         'instructions': '''
-            <h3>Spam detection</h3>
-            <p>Judge whether these text messages are spam or not.</p>
+<h3>Spam detection</h3>
+<p>Judge whether these text messages are spam or not.</p>
         ''',
         'cml': '''
-            {{text}}
-            <cml:radios label="spam" validates="required" gold="true">
-                <cml:radio label="Spam" value="spam"></cml:radio>
-                <cml:radio label="Not spam" value="ham"></cml:radio>
-            </cml:radios>
+{{text}}
+<cml:radios label="Spam?" name="spam" validates="required" gold="true">
+    <cml:radio label="Spam" value="spam"></cml:radio>
+    <cml:radio label="Not spam" value="ham"></cml:radio>
+</cml:radios>
         ''',
         'options': {
             'front_load': 1, # quiz mode = 1; turn off with 0
         }
     })
-    print 'Updated job'
+    print >> sys.stderr, 'Updated job'
 
     # add the 'python-example' tag so that we can easily find this job later
     job.tags = [job_tag]
-    print 'Tagged job as %r' % job_tag
+    print >> sys.stderr, 'Tagged job as %r' % job_tag
 
     # because we used the standard _gold postfix for this field,
     # the second argument is actually unecessary
     job.gold_add('spam', 'spam_gold')
-    print 'Designated gold data'
+    print >> sys.stderr, 'Designated gold data'
 
 
 def launch():
     job = _find_job()
     job.launch(200)
-    print 'Launched Job[%d]' % job.id
+    print >> sys.stderr, 'Launched Job[%d]' % job.id
+
+
+def ping():
+    job = _find_job()
+    print job.ping()
+
+
+def results():
+    job = _find_job()
+    try:
+        for judgment in job.judgments:
+            print '\t'.join([judgment['label'], judgment['spam'], judgment['text']])
+    except CrowdFlowerError, exc:
+        # explain HTTP 202 Accepted response
+        if exc.response.status_code == 202:
+            print >> sys.stderr, 'Try again in a moment', exc
+
+
+def download():
+    job = _find_job()
+    for judgment in job.judgments:
+        print json.dumps(judgment)
 
 
 def delete():
     job = _find_job()
-    print 'Deleting Job[%d]' % job.id
+    print >> sys.stderr, 'Deleting Job[%d]' % job.id
     job.delete()
 
 
 if __name__ == '__main__':
-    import sys
     methods = locals()
     for arg in sys.argv[1:]:
-        print arg, '...'
+        print >> sys.stderr, arg, '...'
         methods[arg]()
