90f5625be0b07d7b7e6dfab6b8575e7fbe80edc1
Christopher Brown <io@henrian.com>
2014-08-22 16:45:17 -0500

    Version bump for job's cache exposure

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='crowdflower',
-    version='0.1.1',
+    version='0.1.2',
     author='Christopher Brown',
     author_email='io@henrian.com',
     url='https://github.com/peoplepattern/crowdflower',
