90bd09ecbc9d96f1d28a2f92962e5a6c72157c41
Christopher Brown <io@henrian.com>
2015-05-31 14:04:20 -0500

    Bump version at patch level

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='crowdflower',
-    version='0.1.2',
+    version='0.1.3',
     author='Christopher Brown',
     author_email='io@henrian.com',
     url='https://github.com/peoplepattern/crowdflower',
